850d11ff9557acd9a7986a349b91fa4e93ae658e
Raul Argueta <rargueta@sanservices.hn>
2020-03-25 02:15:45 -0600

    added test and implementation for ScoreSortingService methods

--- src/main/java/com/jobsity/challenge/service/impl/ScoreSortingServiceImpl.java
+++ src/main/java/com/jobsity/challenge/service/impl/ScoreSortingServiceImpl.java
@@ -1,21 +1,67 @@
 package com.jobsity.challenge.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.jobsity.challenge.domain.Frame;
 import com.jobsity.challenge.domain.Player;
 import com.jobsity.challenge.domain.Score;
 import com.jobsity.challenge.service.ScoreSortingService;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * ScoreSortingServiceImpl
  */
+@Service
+@Slf4j
 public class ScoreSortingServiceImpl implements ScoreSortingService {
 
     @Override
-    public Map<Player, List<Frame>> arrange(List<Score> scores){
-        // TODO Auto-generated method stub
-        return null;
+    public Map<Player, List<Frame>> arrange(List<Score> scores) {
+        Map<Player, List<Frame>> framesByPlayer = new HashMap<>();
+        Map<Player, List<Score>> scoresByPlayer = scores.stream().collect(Collectors.groupingBy(Score::getPlayer));
+        for (Map.Entry<Player, List<Score>> scoreByPlayer : scoresByPlayer.entrySet()) {
+            List<Score> playerScores = scoreByPlayer.getValue();
+
+            List<Frame> playerFrames = new ArrayList<>();
+            // Initialize all 10 frames per player
+            playerFrames.addAll(getFrames());
+            int currentFrame = 1;
+            // loop over scores to assign to corresponding frame.
+            log.debug("Scores: " + playerScores.toString());
+            for (Score playerScore : playerScores) {
+                // get current frame and add the current score
+                Frame frame = playerFrames.get(currentFrame - 1);
+                frame.getScores().add(playerScore);
+                if (currentFrame != 10) {
+                    // if the score was a strike, increase the frame counter and move to next frame
+                    if (frame.getScores().size() == 1 && playerScore.getValue() == 10) {
+                        currentFrame++;
+                    }
+                    // if the current frame has at most 2 scores and its not the last frame, move to
+                    // next frame.
+                    if (frame.getScores().size() == 2) {
+                        currentFrame++;
+                    }
+                }
+            }
+            framesByPlayer.put(scoreByPlayer.getKey(), playerFrames);
+        }
+        return framesByPlayer;
+    }
+
+    private List<Frame> getFrames() {
+        // every match will contain a total of 10 frames per player
+        List<Frame> frames = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            frames.add(new Frame(i, new ArrayList<>()));
+        }
+        return frames;
     }
 }
---
+++ src/test/java/com/jobsity/challenge/ScoreSortingServiceTests.java
@@ -0,0 +1,116 @@
+package com.jobsity.challenge;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.jobsity.challenge.domain.Frame;
+import com.jobsity.challenge.domain.Player;
+import com.jobsity.challenge.domain.Score;
+import com.jobsity.challenge.service.ScoreSortingService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ScoreSortingServiceTests
+ */
+@SpringBootTest
+@Slf4j
+public class ScoreSortingServiceTests {
+
+    @Autowired
+    private ScoreSortingService scoreSortingService;
+
+    @BeforeEach
+    public void init() {
+
+    }
+
+    @Test
+    @DisplayName("TestScoreSortingServiceArrangeWithSampleInput")
+    public void TestScoreSortingServiceArrangeWithSampleInput() {
+
+        Map<Player, List<Frame>> expected = getExpectedScoreSortingServiceArrangeOutput();
+        Map<Player, List<Frame>> actual = scoreSortingService.arrange(getTestScoreListInput());
+
+        log.debug("Expected");
+        System.out.println(expected);
+        log.debug("Result");
+        System.out.println(actual);
+
+        assertEquals(expected, actual);
+
+    }
+
+    private Map<Player, List<Frame>> getExpectedScoreSortingServiceArrangeOutput() {
+        Map<Player, List<Frame>> framesByPlayer = new HashMap<>();
+
+        Player jeff = new Player("Jeff");
+        Player john = new Player("John");
+
+        // initializes both player's total frames
+        framesByPlayer.put(jeff, getFrames());
+        framesByPlayer.put(john, getFrames());
+
+        Frame jeffFrame1 = framesByPlayer.get(jeff).get(0);
+        jeffFrame1.setId(1);
+        jeffFrame1.setScores(Arrays.asList(
+                // Score1
+                new Score(jeff, 10, false)));
+
+        Frame jeffFrame2 = framesByPlayer.get(jeff).get(1);
+        jeffFrame2.setId(2);
+        jeffFrame2.setScores(Arrays.asList(
+                // Score1
+                new Score(jeff, 0, true),
+                // Score2
+                new Score(jeff, 3, false)));
+
+        Frame johnFrame1 = framesByPlayer.get(john).get(0);
+        johnFrame1.setId(1);
+        johnFrame1.setScores(Arrays.asList(
+                // Score1
+                new Score(john, 3, false),
+                // Score2
+                new Score(john, 7, false)));
+
+        return framesByPlayer;
+    }
+
+    private List<Score> getTestScoreListInput() {
+        List<Score> scores = new ArrayList<>();
+
+        Player jeff = new Player("Jeff");
+        Player john = new Player("John");
+
+        scores.add(new Score(jeff, 10, false));
+
+        scores.add(new Score(john, 3, false));
+        scores.add(new Score(john, 7, false));
+
+        scores.add(new Score(jeff, 0, true));
+        scores.add(new Score(jeff, 3, false));
+
+        return scores;
+
+    }
+
+    private List<Frame> getFrames() {
+        // every match will contain a total of 10 frames per player
+        List<Frame> frames = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            frames.add(new Frame(i, new ArrayList<>()));
+        }
+        return frames;
+    }
+}
