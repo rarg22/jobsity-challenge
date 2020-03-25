4f840c5f14f060f01b44d1213102c05957d2c60e
Raul Argueta <rargueta@sanservices.hn>
2020-03-25 03:14:38 -0600

    added Test and implementation for ScoreCalculatorService methods

---
+++ src/main/java/com/jobsity/challenge/service/impl/ScoreCalculatorServiceImpl.java
@@ -0,0 +1,86 @@
+package com.jobsity.challenge.service.impl;
+
+import java.util.List;
+
+import com.jobsity.challenge.domain.Frame;
+import com.jobsity.challenge.service.ScoreCalculatorService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ScoreCalculatorServiceImpl
+ */
+@Service
+public class ScoreCalculatorServiceImpl implements ScoreCalculatorService {
+    @Override
+    public void calculateFrameScores(List<Frame> frames) {
+        int nextFrameId = 1;
+        int totalScore = 0;
+        for (Frame frame : frames) {
+            int totalScorePerFrame = 0;
+            if (frame.getId() != 10) {
+                // if only 1 score, assume its a strike
+                if (frame.getScores().size() == 1) {
+                    totalScorePerFrame = lookAheadWhenStrike(frame, frames, nextFrameId);
+                }
+                // if 2 scores are found, check if Spare or not
+                if (frame.getScores().size() == 2) {
+                    if (checkIfSpare(frame)) {
+                        totalScorePerFrame = lookAheadWhenSpare(frame, frames, nextFrameId);
+                    } else {
+                        totalScorePerFrame += frame.getScores().get(0).getValue();
+                        totalScorePerFrame += frame.getScores().get(1).getValue();
+                    }
+                }
+            } else {
+                // add all 3 frame scores
+                totalScorePerFrame += frame.getScores().get(0).getValue();
+                totalScorePerFrame += frame.getScores().get(1).getValue();
+                totalScorePerFrame += frame.getScores().get(2).getValue();
+            }
+            // set final score for the current frame
+            totalScore += totalScorePerFrame;
+            frame.setTotalScore(totalScore);
+            nextFrameId++;
+        }
+
+    }
+
+    private int lookAheadWhenStrike(Frame frame, List<Frame> frames, Integer nextFrameId) {
+        int totalScore = 0;
+        // get current frame strike score
+        totalScore += frame.getScores().get(0).getValue();
+        // search next
+        Frame nextFrame = frames.get(nextFrameId);
+        if (nextFrame.getScores().size() >= 2) {
+            // add both scores
+            totalScore += nextFrame.getScores().get(0).getValue();
+            totalScore += nextFrame.getScores().get(1).getValue();
+        } else {
+            // add nextFrame only score
+            totalScore += nextFrame.getScores().get(0).getValue();
+            // search frame after next and get the first score found
+            Frame afterNextFrame = frames.get(nextFrameId + 1);
+            totalScore += afterNextFrame.getScores().get(0).getValue();
+        }
+        return totalScore;
+    }
+
+    private int lookAheadWhenSpare(Frame frame, List<Frame> frames, Integer nextFrameId) {
+        int totalScore = 0;
+        // get current frame strike score
+        totalScore += frame.getScores().get(0).getValue();
+        totalScore += frame.getScores().get(1).getValue();
+        // search next
+        Frame nextFrame = frames.get(nextFrameId);
+        // add first score
+        totalScore += nextFrame.getScores().get(0).getValue();
+
+        return totalScore;
+    }
+
+    private boolean checkIfSpare(Frame frame) {
+        return frame.getScores().get(0).getValue() + frame.getScores().get(1).getValue() == 10;
+    }
+
+}
--- src/test/java/com/jobsity/challenge/ScoreCalculatorServiceTests.java
+++ src/test/java/com/jobsity/challenge/ScoreCalculatorServiceTests.java
@@ -1,5 +1,18 @@
 package com.jobsity.challenge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.jobsity.challenge.domain.Frame;
+import com.jobsity.challenge.domain.Player;
+import com.jobsity.challenge.domain.Score;
+import com.jobsity.challenge.service.ScoreCalculatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -8,5 +21,64 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ScoreCalculatorServiceTests {
 
-    
+    @Autowired
+    private ScoreCalculatorService scoreCalculatorService;
+
+    @Test
+    public void TestCalculateFrameScoresGivenSampleInput() {
+        List<Frame> sampleFrameListInput = getSampleFrameListInput();
+
+        List<Frame> expected = getExpectedFrameListOutput();
+        scoreCalculatorService.calculateFrameScores(sampleFrameListInput);
+        assertTrue(expected.containsAll(sampleFrameListInput) && sampleFrameListInput.containsAll(expected));
+    }
+
+    private List<Frame> getExpectedFrameListOutput() {
+        return Arrays.asList(
+                // Frame 1 Scores
+                new Frame(1, Arrays.asList(new Score(10, false)), 20),
+                // Frame 2 Scores
+                new Frame(2, Arrays.asList(new Score(7, false), new Score(3, false)), 39),
+                // Frame 3 Scores
+                new Frame(3, Arrays.asList(new Score(9, false), new Score(0, false)), 48),
+                // Frame 4 Scores
+                new Frame(4, Arrays.asList(new Score(10, false)), 66),
+                // Frame 5 Scores
+                new Frame(5, Arrays.asList(new Score(0, false), new Score(8, false)), 74),
+                // Frame 6 Scores
+                new Frame(6, Arrays.asList(new Score(8, false), new Score(2, false)), 84),
+                // Frame 7 Scores
+                new Frame(7, Arrays.asList(new Score(0, true), new Score(6, false)), 90),
+                // Frame 8 Scores
+                new Frame(8, Arrays.asList(new Score(10, false)), 120),
+                // Frame 9 Scores
+                new Frame(9, Arrays.asList(new Score(10, false)), 148),
+                // Frame 10 Scores
+                new Frame(10, Arrays.asList(new Score(10, false), new Score(8, false), new Score(1, false)), 167));
+    }
+
+    private List<Frame> getSampleFrameListInput() {
+        return Arrays.asList(
+                // Frame 1 Scores
+                new Frame(1, Arrays.asList(new Score(10, false)), 0),
+                // Frame 2 Scores
+                new Frame(2, Arrays.asList(new Score(7, false), new Score(3, false)), 0),
+                // Frame 3 Scores
+                new Frame(3, Arrays.asList(new Score(9, false), new Score(0, false)), 0),
+                // Frame 4 Scores
+                new Frame(4, Arrays.asList(new Score(10, false)), 0),
+                // Frame 5 Scores
+                new Frame(5, Arrays.asList(new Score(0, false), new Score(8, false)), 0),
+                // Frame 6 Scores
+                new Frame(6, Arrays.asList(new Score(8, false), new Score(2, false)), 0),
+                // Frame 7 Scores
+                new Frame(7, Arrays.asList(new Score(0, true), new Score(6, false)), 0),
+                // Frame 8 Scores
+                new Frame(8, Arrays.asList(new Score(10, false)), 0),
+                // Frame 9 Scores
+                new Frame(9, Arrays.asList(new Score(10, false)), 0),
+                // Frame 10 Scores
+                new Frame(10, Arrays.asList(new Score(10, false), new Score(8, false), new Score(1, false)), 0));
+    }
+
 }
